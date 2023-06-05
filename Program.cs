ac7002098cc5973d60a88fdaedfbdf1d46f5a580
Martin Zarutski <martinzar5@gmail.com>
2023-06-05 11:34:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task4
+{
+    class Program
+    {
+        static public void Main()
+        {
+            string[] sweekDays = new string[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+            foreach (string day in sweekDays)
+            {
+                Console.WriteLine(day);
+            }
+
+        }
+    }
+}
